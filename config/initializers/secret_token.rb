d968bc176fda34eb4ea692388a4bd83814e8ec64
Truong Dung <truongdung@developers-iMac.local>
2015-08-14 15:26:37 +0700

    Initialize rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BrowserDetection::Application.config.secret_token = '089a6634b3cc2bbb70327689656319c45783c1a727810ea55d564ad45b6a3e9c0e8ff234af89680d951db516416a358986352aa42af1f5b9381fb0feac4e2d34'
