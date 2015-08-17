3d09b13fdbd1cc91516932929dbc86656a8b85c2
Truong Dung <truongdung@developers-iMac.local>
2015-08-17 13:41:20 +0700

    Fix smtp

--- config/initializers/smtp_settings.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-  :address              => 'smtp.gmail.com',
-  :port                 => 587,
-  :domain               => 'heroku.com',
-  :user_name            => ENV["USERNAME"],
-  :password             => ENV["PASSWORD"],
-  :authentication       => ENV["AUTHENTICATION"],
-  :enable_starttls_auto => true
-}
