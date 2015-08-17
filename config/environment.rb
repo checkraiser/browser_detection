18ca2ec836f0074d6c53652ad67cc5a7267aa056
Truong Dung <truongdung@developers-iMac.local>
2015-08-17 11:59:20 +0700

    fix tls

--- config/environment.rb
+++ config/environment.rb
@@ -1,14 +1,6 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
-ActionMailer::Base.smtp_settings = {
-  :address              => ENV["ADDRESS"],
-  :port                 => ENV["PORT"],
-  :domain               => ENV["DOMAIN"],
-  :user_name            => ENV["USERNAME"],
-  :password             => ENV["PASSWORD"],
-  :authentication       => ENV["AUTHENTICATION"],
-  :enable_starttls_auto => true
-}
+
 
 # Initialize the rails application
 BrowserDetection::Application.initialize!
