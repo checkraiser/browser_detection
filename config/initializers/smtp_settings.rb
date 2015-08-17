c8980337492198ceef554573ce937629792792d3
Truong Dung <truongdung@developers-iMac.local>
2015-08-17 13:38:01 +0700

    Fix env

--- config/initializers/smtp_settings.rb
+++ config/initializers/smtp_settings.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   :address              => 'smtp.gmail.com',
   :port                 => 587,
   :domain               => 'heroku.com',
-  :user_name            => 'checkraiser11@gmail.com',
-  :password             => 'Dung123#',
-  :authentication       => 'plain',
+  :user_name            => ENV["USERNAME"],
+  :password             => ENV["PASSWORD"],
+  :authentication       => ENV["AUTHENTICATION"],
   :enable_starttls_auto => true
 }
