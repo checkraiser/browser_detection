747b64ecdc883cc42511182fdd1b3c32c621298d
Truong Dung <truongdung@developers-iMac.local>
2015-08-19 13:21:49 +0700

    Fix nil

--- app/controllers/my_devise/sessions_controller.rb
+++ app/controllers/my_devise/sessions_controller.rb
@@ -48,7 +48,7 @@ class MyDevise::SessionsController < Devise::SessionsController
   end
 
   def is_changed?(resource, browser, ip, os)
-    Rails.logger.info "is_changed? " + resource.identifier
+    Rails.logger.info "is_changed? " + (resource.identifier || "")
     cookies[:identifier] = Digest::MD5.hexdigest(os + browser + resource.email)
     return false if cookies[:identifier] == resource.identifier
     true
