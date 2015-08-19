000b339a5d9a0fbca976f710a1f923b22f2ac79d
Truong Dung <truongdung@developers-iMac.local>
2015-08-19 12:14:52 +0700

    Fix production email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ BrowserDetection::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  config.action_mailer.default_url_options = { :host => 'https://truongdung1.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'https://truongdung2.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
@@ -72,7 +72,7 @@ BrowserDetection::Application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "truongdung1.herokuapp.com",
+    domain: "truongdung2.herokuapp.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["USERNAME"],
