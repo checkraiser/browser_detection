942da99f6e9fe7f2aa8fa3dc48429dda2dad1608
Truong Dung <truongdung@developers-iMac.local>
2015-08-24 14:52:13 +0700

    Add sign in information

--- Gemfile
+++ Gemfile
@@ -24,7 +24,7 @@ gem 'jquery-rails'
 gem 'rails_12factor', group: :production
 gem 'devise'
 gem 'browser'
-
+gem 'tzinfo'
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,9 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+  def last_sign_in_time
+    vn = TZInfo::Country.get('VN')
+    zone = vn.zone_identifiers.sample
+    TZInfo::Timezone.get(zone).utc_to_local(self.last_sign_in_at)
+  end
 end
